125de17cdbcbf14a4bde50c57605b326e2e9c7fd
lystarDu <lystardu@outlook.com>
2024-09-16 20:36:55 +0800

    hw01

--- homeworks/hw01/hw01.py
+++ homeworks/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = lambda a, b: a - b
     else:
-        f = _____
+        f = lambda a, b: a + b
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i * i + j * j + k * k - max(i, j, k) ** 2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -65,7 +65,11 @@ def largest_factor(n):
     >>> largest_factor(13) # factor is 1 since 13 is prime
     1
     """
-    "*** YOUR CODE HERE ***"
+    largest_factor = 1
+    for i in range(1, n):
+        if n % i == 0:
+            largest_factor = i
+    return largest_factor
 
 
 def hailstone(n):
@@ -87,5 +91,14 @@ def hailstone(n):
     >>> b
     1
     """
-    "*** YOUR CODE HERE ***"
+    print(n)
+    length = 1
+    while (n != 1):
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        print(n)
+        length += 1
+    return length
 
